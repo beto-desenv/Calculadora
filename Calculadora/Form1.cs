7d39556f9f6f2b0cea3f60035bdb5f3b013074cb
Roberto Vieira Carlos <beto.vieiracarlos@gmail.com>
2021-09-06 15:53:50 -0300

    Commit inicial

---
+++ Calculadora/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        decimal valor1 = 0, valor2 = 0;
+        string operacao = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "7";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+
+            txtResultado.Text = txtResultado.Text + "0";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "1";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "2";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "3";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "4";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "5";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "6";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "9";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + ".";
+        }
+
+        //BOTÃO DE "IGUAL"(=)
+        private void button16_Click(object sender, EventArgs e)
+        {
+            valor2 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+
+            if (operacao == "SOMA")
+            {
+                txtResultado.Text = Convert.ToString(valor1 + valor2);
+            }
+            else if (operacao == "SUB")
+            {
+                txtResultado.Text = Convert.ToString(valor1 - valor2);
+            }
+            else if (operacao == "MULTI")
+            {
+                txtResultado.Text = Convert.ToString(valor1 * valor2);
+            }
+            else
+            {
+                txtResultado.Text = Convert.ToString(valor1 / valor2);
+            }
+        }
+
+        //BOTÃO DE "SUBTRAÇÃO"(-)
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "SUB";
+                lblOperacao.Text = "-";
+            }
+            else
+            {
+                MessageBox.Show("Informe um valor para efetuar a SUBTRAÇÃO.");
+            }
+        }
+
+        //BOTÃO DE "MULTIPLICAÇÃO"(X)
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "MULTI";
+                lblOperacao.Text = "x";
+            }
+            else
+            {
+                MessageBox.Show("Informe um valor para efetuar a MULTIPLICAÇÃO.");
+            }
+        }
+
+        //BOTÃO DE "DIVISÃO"(/)
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "DIV";
+                lblOperacao.Text = "/";
+            }
+            else
+            {
+                MessageBox.Show("Informe um valor para efetuar a DIVISÃO.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "";
+            valor1 = 0;
+            valor2 = 0;
+            lblOperacao.Text = "";
+        }
+
+        //BOTÃO DE "SOMA"(+)
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "SOMA";
+                lblOperacao.Text = "+";
+            }
+            else
+            {
+                MessageBox.Show("Informe um valor para efetuar a SOMA.");
+            }
+        }
+    }
+}
